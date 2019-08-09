0e2add7a34168f54d2494c1d2f05ff8aa637072a
Tushar Agarwal <tagarwal@redhat.com>
2019-08-09 14:28:48 +0530

    select statement

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Table, MetaData, Integer, String, ForeignKey, create_engine
+from sqlalchemy import select
 
 engine = create_engine("sqlite:///:memory:",echo=True) #using the inmemory sql database, also echo for outputing the raw sql statements
 metadata = MetaData()
@@ -29,3 +30,20 @@ con.execute(ins, [
 {"name" : "foo 3","full_name":"foo bar 3"},
 {"name" : "foo 4","full_name":"foo bar 4"}
 ]) #executing the bulk insert using multiple data dictionaries in the execute statement
+
+#select statements
+
+select = select([users.c.name,users.c.full_name]) #specify the columns using the c attribute of the table object where columns of the table are the attributes of the c object
+result = con.execute(select)
+for row in result:
+    print("name: %s, full_name: %s"% (row["name"],row["full_name"])) #print the values in the row by the named columns we can also use the indexes like
+
+result.close() #its good to close the result to discard the pending rows
+
+result = con.execute(select)
+for row in result:
+    print("name: %s, full_name: %s"% (row[0], row[1])) # print the rows using the indexes
+
+
+result.close()
+
