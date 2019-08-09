0fe94a108b19b50d6be2b5fc75ab2abf31f616ea
Tushar Agarwal <tagarwal@redhat.com>
2019-08-09 14:07:34 +0530

    bulk insert

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Table, MetaData, Integer, String, ForeignKey, create_engine
 
-engine = create_engine("sqlite:///:memory:",echo=True)
+engine = create_engine("sqlite:///:memory:",echo=True) #using the inmemory sql database, also echo for outputing the raw sql statements
 metadata = MetaData()
 
 users = Table("users",metadata,
@@ -19,7 +19,13 @@ metadata.create_all(engine) #method to create all the tables
 
 #executing the insert statement
 
-ins = users.insert().values(name = "foo",full_name = "foo bar") #targeting the users table while providing the values
+ins = users.insert()#targeting the users table while providing the values
 
 con = engine.connect() #connecting to the database actually
-con.execute(ins) #executing the insert statement
+con.execute(ins, [
+{"name" : "foo","full_name":"foo bar"},
+{"name" : "foo 1","full_name":"foo bar 1"},
+{"name" : "foo 2","full_name":"foo bar 2"},
+{"name" : "foo 3","full_name":"foo bar 3"},
+{"name" : "foo 4","full_name":"foo bar 4"}
+]) #executing the bulk insert using multiple data dictionaries in the execute statement
